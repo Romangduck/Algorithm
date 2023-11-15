aad801b7e9410cc5a4a1e6277905e3e57977a665
Romangduck <129341526+Romangduck@users.noreply.github.com>
2023-11-16 07:38:43 +0900

    [level unrated] Title: 중앙값 구하기, Time: 0.47 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array) {
+        
+        
+        Arrays.sort(array);
+        
+        int answer = array.length /2;
+        
+        return array[answer];
+    }
+}
